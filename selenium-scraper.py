954eac7dbba0c2b9110055a65095234164d196f0
kevalrajpalknight <kevalprogram100@gmail.com>
2021-02-10 11:40:17 +0530

    Adjusted Code for More Generic Use

--- selenium-scraper.py
+++ selenium-scraper.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 import time
 driver = webdriver.Firefox()
-
-with open("vinod-khosla-videos.txt", "w") as file:
+name = "anu-hariharan"
+with open(f"{name}-videos.txt", "w") as file:
      
-    driver.get(f"https://waitwho.is/vinod-khosla/videos")
+    driver.get(f"https://waitwho.is/{name}/videos")
     time.sleep(4)
     results = driver.find_elements_by_xpath("//iframe")
     for iframe in results:
